dd3eaae633774fbd920a87321a629b942130c009
Weverson Nascimento <weverson.sn@gmail.com>
2020-01-13 01:31:59 -0300

    Add function to play the game

--- src/tic_tac_toe/game.clj
+++ src/tic_tac_toe/game.clj
@@ -1,7 +1,26 @@
 (ns tic-tac-toe.game
-  (:gen-class))
+  (:require [tic-tac-toe.board :as board]
+            [tic-tac-toe.player :as player]
+            [clojure.pprint :as p]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "hello world!"))
+(def empty-state
+  {:board board/empty-board
+   :player player/player-1})
+
+(def state
+  (atom empty-state))
+
+(defn start!
+  []
+  (swap! state merge empty-state))
+
+(defn play!
+  [position]
+  (let [{:keys [board player]} @state
+        updated-board          (board/update-board board position player)
+        next-player            (player/switch-player player)
+        player-won?            (player/won? updated-board player)]
+    (cond
+      (nil? updated-board) "the position passed not is valid, plase try again"
+      (true? player-won?)  (str "The player " player " is the winner!")
+      :else                (swap! state merge {:board updated-board :player next-player}))))
--- test/tic_tac_toe/game_test.clj
+++ test/tic_tac_toe/game_test.clj
@@ -1,7 +1,19 @@
 (ns tic-tac-toe.game-test
   (:require [clojure.test :refer :all]
-            [tic-tac-toe.game :refer :all]))
+            [tic-tac-toe.game :as game]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 1 1))))
+(deftest play!
+  (testing "player 1 should win the game"
+    (game/start!)
+    (game/play! [0 0])
+    (game/play! [0 1])
+    (game/play! [1 1])
+    (game/play! [0 2])
+    (is (= "The player :x is the winner!" (game/play! [2 2])))))
+
+(deftest play!
+  (testing "should return a error when position is invalid"
+    (game/start!)
+    (game/play! [0 0])
+    (is (= "the position passed not is valid, plase try again" (game/play! [-1 0])))
+    (is (= "the position passed not is valid, plase try again" (game/play! [0 0])))))
